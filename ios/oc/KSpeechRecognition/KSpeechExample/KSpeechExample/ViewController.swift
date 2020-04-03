0825a4551b1a6f099393ed2db735654587168f6b
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-03 17:38:31 +0800

    增加测试demo

--- ios/oc/KSpeechRecognition/KSpeechExample/KSpeechExample/ViewController.swift
+++ ios/oc/KSpeechRecognition/KSpeechExample/KSpeechExample/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+        
+    //let recognizer = KSpeechRecognizer(language: .chinese, shouldReportPartialResults: true)
+
+    @IBOutlet weak var logView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func recognitionButtonTapped(_ sender: UIButton) {
+        let selected = !sender.isSelected
+        sender.isSelected = selected
+        if selected {
+//            recognizer.start(resultHandler: { [weak self] (result) in
+//                self?.outputLog("识别结果：\(result)")
+//            }) { [weak self] (error) in
+//                self?.outputLog("识别出错: \(error)")
+//            }
+        } else {
+            //recognizer.stop()
+        }
+    }
+    
+    @IBAction func clearLog(_ sender: Any) {
+        logView.text = nil
+    }
+    
+    private func outputLog(_ message: String) {
+        var stringBuffer = logView.text ?? ""
+        stringBuffer.append(contentsOf: "\n\(message)")
+        DispatchQueue.main.async {
+            self.logView.text = stringBuffer
+        }
     }
 
 
